4765626b592ed5de34caf8c55cc1635da3ec87a6
Kim Jangwook <kim.jangwook.1029@gmail.com>
2022-02-11 13:24:19 +0900

    최초 커밋

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/**
+ * 리액트 사용이 필요할 경우 코멘트 아웃 제거
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +12,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ */
